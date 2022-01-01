4546ddfc518a8f424d7932091dc9e0d9a4cb4410
Tsyuay <83605971+Tsyuay@users.noreply.github.com>
2021-12-31 23:53:52 -0300

    Add files via upload

---
+++ newyearrescue_2021/Extensions/Physics2Behavior/physics2tools.js
@@ -0,0 +1,2 @@
+var gdjs;(function(e){let r;(function(a){a.objectsCollide=function(s,o,n,t){return e.evtTools.object.twoListsTest(e.Physics2RuntimeBehavior.collisionTest,s,n,t,o)},a.setTimeScale=function(s,o,n){const t=e.staticArray(e.physics2.setTimeScale);s.values(t);for(let i=0,m=t.length;i<m;i++){const c=t[i];for(let l=0,u=c.length;l<u;l++){e.Physics2RuntimeBehavior.setTimeScaleFromObject(c[l],o,n);return}}}})(r=e.physics2||(e.physics2={}))})(gdjs||(gdjs={}));
+//# sourceMappingURL=physics2tools.js.map
